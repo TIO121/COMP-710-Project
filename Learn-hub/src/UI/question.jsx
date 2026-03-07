3d0b3990d8a5e7841a82fbb6aa1c37ca86cd56a7
NicolasHarris <npharris@aggies.ncat.edu>
2026-03-07 15:29:21 -0500

    Added Ask Question Page

--- Learn-hub/src/UI/question.jsx
+++ Learn-hub/src/UI/question.jsx
@@ -1,18 +1,23 @@
+import { useState } from "react"
+import "./question.css"
+
+
 export function AskQuestion(){
+
     return (
+
         <>
             <section class="heading">
                 <h1> Ask a Question</h1>
-                <p1>Get instant answers to your study questions</p1>
-                 <div class ="Toggle">
-                    <label >Ask Question</label>
-                    <label>Browse Q/A</label>
-                </div>
+                <p>Get instant answers to your study questions</p>
             </section>
 
             <section class ="Ask-Question">
                 <h2>Ask Your Question</h2>
-                <input type="text" name="questionField" id="question" placeholder="Type your Question here.... (e.g., What are Derivatives?)" />
+                <div class ="questionInput">
+                    <input  maxLength ="500" name="questionField" id="question" placeholder="Type your Question here.... (e.g., What are Derivatives?)" columns="30" rows ="5"></input>
+                    <button> Ask</button>
+                </div>
                 <div class ="upload">
                     <img src="" alt="" />
                     <h3>Upload a picture of your question</h3>
@@ -23,7 +28,7 @@ export function AskQuestion(){
             </section>
             <section class =" popular-questions">
                 <h3>Popular Questions</h3>
-                <div>
+                <div class='questionCard'>
                     <p>What is pythagerean therorem?</p>
                     <label >Mathematices</label>
                 </div>
